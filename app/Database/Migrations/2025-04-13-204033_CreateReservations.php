78641972b9f07e43bf47b55c1279760d309eb1e5
Gilberto <gilbertonerigodoy@gmail.com>
2025-04-13 21:03:49 -0300

    Criando migration, model, entity das reservas

---
+++ app/Database/Migrations/2025-04-13-204033_CreateReservations.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateReservations extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField(
+            [
+                'id' => [
+                    'type'           => 'INT',
+                    'constraint'     => 11, 
+                    'unsigned'       => true,
+                    'auto_increment' => true,
+                ],
+                'resident_id' => [
+                    'type'           => 'INT',
+                    'constraint'     => 11, 
+                    'unsigned'       => true,
+                    'null'           => true,
+                    'default'        => null,
+                ],
+                'area_id' => [
+                    'type'           => 'INT',
+                    'constraint'     => 11, 
+                    'unsigned'       => true,
+                    'null'           => true,
+                    'default'        => null,
+                ],
+                'code' => [
+                    'type'           => 'INT',
+                    'constraint'     => 8,                    
+                    'null'           => false,                    
+                ],
+                'desired_date' => [
+                    'type'           => 'DATETIME',                    
+                    'comment'        => 'Data e Hora desejada para iniciar o uso da área',                    
+                ],
+                'status' => [
+                    'type'           => 'VARCHAR',
+                    'constraint'     => 255,                    
+                    'null'           => false,                    
+                ],
+                'reason_status' => [
+                    'type'           => 'VARCHAR',
+                    'constraint'     => 70,                    
+                    'comment'        => 'Detalhes do status e etc',                    
+                ],
+                'notes' => [
+                    'type'           => 'VARCHAR',
+                    'constraint'     => 255,                    
+                    'null'           => false,  
+                    'default'        => null,                    
+                ],                
+                'created_at' => [
+                    'type'           => 'DATETIME',
+                    'null'           => true,
+                    'default'        => null,                    
+                ],
+                'updated_at' => [
+                    'type'           => 'DATETIME',
+                    'null'           => true,
+                    'default'        => null,                    
+                ],
+            ]
+        );
+
+        $this->forge->addKey('id', true);
+        $this->forge->addKey('resident_id');
+        $this->forge->addKey('area_id');
+        $this->forge->addKey('code');
+        $this->forge->addKey('desired_date');
+        $this->forge->addKey('status');       
+        $this->forge->addKey('created_at');
+
+        $this->forge->addForeignKey('resident_id', 'residents', 'id', 'CASCADE', 'CASCADE');
+        $this->forge->addForeignKey('area_id', 'areas', 'id', 'CASCADE', 'CASCADE');
+
+        $this->forge->createTable('reservations');
+
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('reservations');
+    }
+}
---
+++ app/Models/ReservationModel.php
@@ -0,0 +1,30 @@
+<?php
+// cSpell:disable
+namespace App\Models;
+
+use App\Entities\Reservation;
+use App\Models\Basic\AppModel;
+
+
+
+class ReservationModel extends AppModel
+{
+    protected $table            = 'reservationss';    
+    protected $returnType       = Reservation::class;   
+    protected $allowedFields    = [        
+        'area_id',
+        'resident_id',
+        'notes',    
+        'status',
+        'reason_status',
+        'desired_date',    
+    ];   
+
+    protected function relateData(object &$area, array $contains = []): void
+    {
+        if(in_array('reservations', $contains)) {
+           //TODO: Recuperar as reservas que cada área possui.
+        }
+    }
+
+}
