6e7dc80b8db2a853ae7e352b9fbfb78b2176ef59
Gilberto <gilbertonerigodoy@gmail.com>
2025-04-15 21:52:38 -0300

    Model Reservation parte 3 final

--- app/Models/ReservationModel.php
+++ app/Models/ReservationModel.php
@@ -38,15 +38,75 @@ class ReservationModel extends AppModel
         $data['data'] ['resident_id']   = auth()->user()->resident->id;
 
         return $data;
-
        
     }
 
-    protected function relateData(object &$area, array $contains = []): void
+    public function all(): array 
     {
-        if(in_array('reservations', $contains)) {
-           //TODO: Recuperar as reservas que cada área possui.
+
+        $this->whereResident();
+
+        $this->select([
+            'reservations.*',
+            'areas.name As area',
+        ]);
+
+        $this->join('areas', 'areas.id = reservations.area_id');
+
+        $this->orderBy('reservations.created_at', 'DESC');
+
+        return $this->findAll();
+    }
+
+    public function getByCode(string $code, array $contains = []): object
+    {
+        $this->whereResident();
+
+        // Recupera a reserva pelo código
+        $reservation = parent::getByCode(code: $code);
+
+        // Relaciona dados adicionais, se necessário
+        $this->relateData($reservation, $contains);
+
+        return $reservation;
+    }
+    
+
+
+    protected function relateData(object &$reservation, array $contains = []): void
+    {
+        if(in_array('bill', $contains)) {
+           //TODO: buscar a cobrançaassociada à reserva.
+
+           //$reservation->bill = $this->billModel->getByCode($reservation->bill_code);
         }
+
+        if(in_array('resident', $contains)) {
+             
+            $reservation->reservation = model(ResidentModel::class)
+                ->where('id', $reservation->resident_id)->first();
+         }
+
+         if(in_array('area', $contains)) {
+             
+            $reservation->area = model(AreaModel::class)
+                ->where('id', $reservation->area_id)->first();
+         }
+
+
+    }
+
+    public function markAs(string $code, Status $status): bool
+    {
+
+        $data = [
+            'status' => $status->value, 
+            'reason_status' => $status->label()
+        ];
+
+        return $this->set($data)
+            ->where('code', $code)
+            ->update();
     }
 
 }
